87c069cc99b0a4f76e8020b21654751973e88772
Mojowlekaz <mojowlekaz@gmail.com>
2023-01-03 09:35:15 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './Components/Navbar';
+import Mainpage from './Components/Mainpage';
+
+function App() {
+  return (
+    <div className="">
+      <Navbar />
+      <Mainpage />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <div className='Navbar'>
+      <h1>Void Feathers</h1>
+      <a href=''>discord</a>
+      <a href='' > Twitter</a>
+      <div className='Nav-Content'>
+      </div>
+    </div>
+  )
+}
